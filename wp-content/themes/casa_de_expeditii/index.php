5ac9b11e401c241d8d9a261ce929e38a1f903f25
mirceatodor <mirceatodor7@gmail.com>
2021-07-12 23:49:10 +0300

    Git commit --m "WP home testimonials & refine sections, hero spacing"

--- wp-content/themes/casa_de_expeditii/index.php
+++ wp-content/themes/casa_de_expeditii/index.php
@@ -1,12 +1,10 @@
-<?php get_header() ?>      *Casa de Expeditii* Content area  
+<?php get_header() ?>
 
 <?php get_template_part('template-parts/homepage/hero-top'); ?>
 <?php get_template_part('template-parts/homepage/services'); ?>
 <?php get_template_part('template-parts/homepage/hero-leading'); ?>
 <?php get_template_part('template-parts/homepage/hero-download'); ?>
+<?php get_template_part('template-parts/homepage/testimonials'); ?>
 <?php get_template_part('template-parts/homepage/blog'); ?>
 
 <?php get_footer() ?>
-
-<!--pt ca header /footer trebuie sa apara pe toate paginile temei, ele se extrag si se includ ulterior in toate fisierele-->
-<!--le extragem aici, iar cand trebuie sa modificam ceva la ele, modificam o data si se schimba in toate paginile--> 
