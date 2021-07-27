35183863d724a7fa67c6d979e3e585e1ad2f72b5
mirceatodor <mirceatodor7@gmail.com>
2021-07-27 19:05:10 +0300

    blog page + single blog post + carbonfieds + refine

--- wp-content/themes/casa_de_expeditii/footer.php
+++ wp-content/themes/casa_de_expeditii/footer.php
@@ -2,38 +2,29 @@
 <footer class="footer">
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-md-5 col-lg-4">
-            <a href="/"><img src="<?= get_stylesheet_directory_uri() . "/images/memologoblack.png" ?>" /></a>
-            <p>Memo Transporter oferă servicii de transport marfă Intern sau Extern</p>
-            <p>©MEMO Transporter LTD 2021. Toate drepturile rezervate</p>
-            </div>
-            <div class="footer-links col-xs-12 col-sm-4 col-md-2 col-md-offset-1 col-lg-offset-2">
-                <strong>Compania</strong>
-                <ul>
-                <li><a href="#">Despre noi</a></li>
-                <li><a href="#">Testimoniale</a></li>
-                <li><a href="#">Caută un transportator</a></li>
-                <li><a href="#">Cerere de ofertă</a></li>
-                </ul>
-            </div>
-            <div class="footer-links col-xs-12 col-sm-4 col-md-2">
-                <strong>Regiuni</strong>
-                <ul>
-                <li><a href="#">România</a></li>
-                <li><a href="#">Europa de Est</a></li>
-                <li><a href="#">Europa Centrală</a></li>
-                <li><a href="#">Europa de Vest</a></li>
-                </ul>
-            </div>
-            <div class="footer-links col-xs-12 col-sm-4 col-md-2">
-                <strong>Suport</strong>
-                <ul>
-                <li><a href="#">Centrul de suport</a></li>
-                <li><a href="#">Contact</a></li>
-                <li><a href="#">Instrucțiuni</a></li>
-                <li><a href="#">Cum functionează</a></li>
-                </ul>
-            </div>
+            <?php if (is_active_sidebar('footer-1')): ?>
+                <div class="col-xs-12 col-md-5 col-lg-4 footer-about">
+                    <?php dynamic_sidebar('footer-1'); ?>
+                </div>
+            <?php endif ?>
+
+            <?php if (is_active_sidebar('footer-2')): ?>
+                <div class="footer-links col-xs-12 col-sm-4 col-md-2 col-md-offset-1 col-lg-offset-2">
+                    <?php dynamic_sidebar('footer-2'); ?>
+                </div>
+            <?php endif ?>
+
+            <?php if (is_active_sidebar('footer-3')): ?>
+                <div class="footer-links col-xs-12 col-sm-4 col-md-2">
+                    <?php dynamic_sidebar('footer-3'); ?>
+                </div>
+            <?php endif ?>
+
+            <?php if (is_active_sidebar('footer-4')): ?>
+                <div class="footer-links col-xs-12 col-sm-4 col-md-2">
+                    <?php dynamic_sidebar('footer-4'); ?>
+                </div>
+            <?php endif ?>
         </div>
     </div>
 </footer>
